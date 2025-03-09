dccb4c6a60268d2ce1350cf00e8e7e5bfaf1a428
JabeenAshique <jbjaby@gmail.com>
2025-03-09 11:35:33 +0530

    exploring httpmethods on routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 
 const app = express()
-
-app.use("/test",(req,res)=>{
-    res.send("hello from server")    
+// order of routes is mandatory!!!
+app.get("/test",(req,res)=>{
+    res.send("hello from server test")    
 })
-app.use("/hello",(req,res)=>{
-    res.send("hello")    
+app.post("/test",(req,res)=>{
+    res.send("post success")    
+})
+//this will match all the http methods in /test 
+app.use("/test",(req,res)=>{
+    res.send(" use checking")    
+})
+app.use("/",(req,res)=>{
+    res.send("hello from server abc")    
 })
 app.listen(3000,()=>{
     console.log("Server sucessfully running... on PORT 3000");
